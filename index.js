4fc24e321daa882169e1dc0be901749fdc382b9c
shakib2123 <mshakibalhasan21@gmail.com>
2023-10-18 13:31:12 +0600

    samsung data get method added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,13 +42,23 @@ async function run() {
       res.send(result);
     });
 
-
-    app.get('/products', async (req, res) =>
-    {
+    app.get("/products", async (req, res) => {
       const result = await productCollection.find().toArray();
-      res.send(result)
-    })
-  
+      res.send(result);
+    });
+
+    app.get("/products/samsung", async (req, res) => {
+      const brand = "Samsung";
+      const query = { brand: brand };
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/products/samsung/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/products", async (req, res) => {
       const product = req.body;
